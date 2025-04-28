a4a1eada8c5783f39f72805671abfdd87f702b4f
Tim van der Lippe <TimvdLippe@users.noreply.github.com>
2025-04-28 09:57:56 +0200

    Update companyURL voor Logius (#5)
    Relates to Logius-standaarden/Automatisering#23

--- js/config.js
+++ js/config.js
@@ -15,7 +15,7 @@ let respecConfig = {
       {
         name: "Logius Standaarden",
         company: "Logius",
-        companyURL: "https://github.com/Logius-standaarden",
+        companyURL: "https://logius.nl",
       }
     ],
   authors:
@@ -23,7 +23,7 @@ let respecConfig = {
       {
         name: "Logius Standaarden",
         company: "Logius",
-        companyURL: "https://github.com/Logius-standaarden",
+        companyURL: "https://logius.nl",
       }
     ],
   github: "https://github.com/Logius-standaarden/logboek-extensie-nen7513",
